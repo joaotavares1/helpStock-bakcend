445b8cc8df6b0c9af32138b127919d62c377ea77
joaotavares1 <joao.andrade30@fatec.sp.gov.br>
2024-11-11 12:40:30 -0300

    Aula 1111 - Camada de Aplicação com DTO

---
+++ HelpStockApp/HelpStockApp.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using HelpStockApp.Application.DTOs;
+
+namespace HelpStockApp.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetAllCategoryById(int? id);
+        Task Add(CategoryDTO categoryDto);
+
+        Task Update(CategoryDTO categoryDto);
+
+        Task Remove(int? id);
+
+    }
+}
